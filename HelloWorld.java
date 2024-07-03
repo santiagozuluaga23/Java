e3093f1caf1c3ec0d95c0a4a25872790da212b3e
santiagozuluaga23 <164108969+santiagozuluaga23@users.noreply.github.com>
2024-07-02 21:33:57 -0500

    Add files via upload

---
+++ HelloWorld.java
@@ -0,0 +1,7 @@
+public class HelloWorld {
+    public static void main(String[] arg){
+        String name = "Santiago";
+        String lastname = "Zuluaga";
+        System.out.println("Hello " + name + " " + lastname + ", esta es tu primera linea de código en Java, con toda");
+    }
+}
